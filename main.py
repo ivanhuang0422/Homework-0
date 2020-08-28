55fd15670369842903f58a07d35577afbdba78b9
YuHsiang Huang <ykh5222@psu.edu>
2020-08-28 22:38:05 +0000

    modified the code second time

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-Celsius = float(input('Enter temperature in Celcius: '))
+Celsius = float(input("Enter temperature in Celcius: "))
 Farenheit= (9/5*Celsius) + 32
 print (f" {Celsius, chr(176)} in Celsius is equivilant to {Farenheit, chr(176)}  Farenheit")
