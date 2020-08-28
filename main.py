b12ca40d30452804dfe6dbb4ac48fbae17aba81b
YuHsiang Huang <70194804+ivanhuang0422@users.noreply.github.com>
2020-08-29 07:48:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Celsius= float(input("Enter temperature in celsius: "))
 Fahrenheit= (9/5*Celsius) + 32 
-print (f" {Celsius}{chr(176)} in Celsius is equivalent to {Fahrenheit}{chr(176)} Fahrenheit.")
+print (f"{Celsius}{chr(176)} in Celsius is equivalent to {Fahrenheit}{chr(176)} Fahrenheit.")
