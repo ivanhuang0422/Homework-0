0b580c2d95a7f05a25731da49f56d956d1149e81
YuHsiang Huang <ykh5222@psu.edu>
2020-08-28 23:05:08 +0000

    Add spaces between numbers and words.

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Celsius= float(input("Enter temperature in celsius: "))
 Farenheit= (9/5*Celsius) + 32 
-print (f"{Celsius}{chr(176)}in Celsius is equivalent to {Farenheit}{chr(176)}Farenheit.")
+print (f"{Celsius}{chr(176)} in Celsius is equivalent to {Farenheit}{chr(176)} Farenheit.")
